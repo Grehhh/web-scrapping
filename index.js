3d543618d8df8055c4ed8a3e7f40b3d165e70a61
Graham <graham.marengo.contractor@bbva.com>
2021-04-14 22:04:47 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://grehhh.github.io/lorem_store_sass/');
+    const title = await page.$('h1');
+
+    console.log(await page.evaluate(el => el.textContent, title));
+
+    await browser.close();
+})
