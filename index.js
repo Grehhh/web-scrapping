171c246145ced9a81f08fc7169427f5903e2bb39
Graham <grh.mrg@gmail.com>
2021-10-14 22:57:12 +0200

    de momento funciona

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
+let rawData = fs.readFileSync('./links.json')
+let links = JSON.parse(rawData);
+let json = {
+    name: 'hola',
+    link: 'por determinar'
+}
+let toJson = JSON.stringify(json);
+fs.writeFileSync('./links.json',toJson);
+
+console.log(links);
 
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://grehhh.github.io/lorem_store_sass/');
-    const title = await page.$('h1');
+    // await page.goto('https://grehhh.github.io/lorem_store_sass/');
+    await page.goto('https://southparkspainhd.wordpress.com/');
+    hrefs = await page.$$eval('a', as => as.map(a => a.href));
 
-    console.log(await page.evaluate(el => el.textContent, title));
+    // console.log(hrefs)
+
+
 
     await browser.close();
 })();
+
+
+// let nums = [];
+// for(let i = 0; i < 5; i++) {
+//     // nums.push(`<li>${i}</li>`)
+//     nums += `<li>${i}</li>`
+// }
+// let palHTML = `<ul>${nums}</ul>`
+// document.body.innerHTML = palHTML;
+// console.log(nums)
+// console.log(palHTML);
