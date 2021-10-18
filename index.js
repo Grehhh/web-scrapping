32b170b6a620ba5d294a50875dc8b7ea09c7a53b
Graham <grh.mrg@gmail.com>
2021-10-18 13:07:32 +0200

    added front, json, style

--- index.js
+++ index.js
@@ -1,37 +1,25 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-let rawData = fs.readFileSync('./links.json')
-let links = JSON.parse(rawData);
-let json = {
-    name: 'hola',
-    link: 'por determinar'
-}
-let toJson = JSON.stringify(json);
-fs.writeFileSync('./links.json',toJson);
-
-console.log(links);
+//----para lectura
+// let rawData = fs.readFileSync('./links.json')
+// let links = JSON.parse(rawData);     
+//----------------
+let linksToJson = [];
 
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    // await page.goto('https://grehhh.github.io/lorem_store_sass/');
     await page.goto('https://southparkspainhd.wordpress.com/');
     hrefs = await page.$$eval('a', as => as.map(a => a.href));
-
-    // console.log(hrefs)
-
-
+    for(let i = 0; i < hrefs.length; i++) {
+        linksToJson.push({
+            name: i.toString(),
+            url: hrefs[i]
+        })
+    };
+    let toJson = JSON.stringify(linksToJson);
+    fs.writeFileSync('./links.json',toJson);        //para escritura
 
     await browser.close();
 })();
 
-
-// let nums = [];
-// for(let i = 0; i < 5; i++) {
-//     // nums.push(`<li>${i}</li>`)
-//     nums += `<li>${i}</li>`
-// }
-// let palHTML = `<ul>${nums}</ul>`
-// document.body.innerHTML = palHTML;
-// console.log(nums)
-// console.log(palHTML);
