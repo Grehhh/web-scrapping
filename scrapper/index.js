f2a1478e97a539a998771f44f640f6cd4eb6c645
Graham <graham.marengo.contractor@bbva.com>
2021-11-11 17:01:39 +0100

    tengo prisa

--- scrapper/index.js
+++ scrapper/index.js
@@ -3,11 +3,35 @@ const fs = require('fs');
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-//----para lectura
-// let rawData = fs.readFileSync('./links.json')
-// let links = JSON.parse(rawData);     
-//----------------
-let linksToJson = [];
+
+const port = 3000;
+const uri = 'mongodb+srv://Graham:De4throcker@cluster0.xmw07.mongodb.net/testDB1?retryWrites=true&w=majority'
+mongoose.connect(uri, (err) => {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log('Conectado a ' + uri);
+    }
+}, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+});
+
+app.listen(port, () => {
+    console.log('App listening on port ' + port);
+})
+
+
+const Schema = new mongoose.Schema;
+const linkSchema = new Schema({
+    season: String,
+    name: String,
+    url: String
+})
+const linkModel = mongoose.model('link', linkSchema);
+
+const linksToDB = [];
+
 
 (async () => {
     const browser = await puppeteer.launch();
@@ -15,14 +39,18 @@ let linksToJson = [];
     await page.goto('https://southparkspainhd.wordpress.com/');
     const names = await page.$$eval('.menu-item-object-post', name => name.map(n => n.textContent));
     const hrefs = await page.$$eval('.menu-item-object-post > a', as => as.map(a => a.href));
-    for(let i = 0; i < hrefs.length; i++) {
-        linksToJson.push({
+    for(let i = 0; i < hrefs.length; i++) {d
+        linksToDB.push({
             name: names[i],
             url: hrefs[i]
         })
     };
-    const toJson = JSON.stringify(linksToJson);
-    fs.writeFileSync('./links.json',toJson);        //para escritura
+
+    app.post('/links', (req,res) => {
+
+    })
+    
+
     // console.log(names)
     // console.log(hrefs)
     await browser.close();
