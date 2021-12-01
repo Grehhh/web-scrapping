38bc3ee2aff84b558a7922bfb722bad2c048c245
Graham <grh.mrg@gmail.com>
2021-12-01 21:25:14 +0100

    corrections

--- scrapper/index.js
+++ scrapper/index.js
@@ -3,35 +3,7 @@ const fs = require('fs');
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-
 const port = 3000;
-const uri = 'mongodb+srv://Graham:De4throcker@cluster0.xmw07.mongodb.net/testDB1?retryWrites=true&w=majority'
-mongoose.connect(uri, (err) => {
-    if(err) {
-        console.log(err);
-    } else {
-        console.log('Conectado a ' + uri);
-    }
-}, {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
-});
-
-app.listen(port, () => {
-    console.log('App listening on port ' + port);
-})
-
-
-const Schema = new mongoose.Schema;
-const linkSchema = new Schema({
-    season: String,
-    name: String,
-    url: String
-})
-const linkModel = mongoose.model('link', linkSchema);
-
-const linksToDB = [];
-
 
 (async () => {
     const browser = await puppeteer.launch();
@@ -39,20 +11,45 @@ const linksToDB = [];
     await page.goto('https://southparkspainhd.wordpress.com/');
     const names = await page.$$eval('.menu-item-object-post', name => name.map(n => n.textContent));
     const hrefs = await page.$$eval('.menu-item-object-post > a', as => as.map(a => a.href));
+    const linksToDB = [];
     for(let i = 0; i < hrefs.length; i++) {d
         linksToDB.push({
             name: names[i],
             url: hrefs[i]
         })
     };
-
-    app.post('/links', (req,res) => {
-
-    })
+    const uri = 'mongodb+srv://Graham:lacontrasenya@cluster0.xmw07.mongodb.net/testDB1?retryWrites=true&w=majority'
+    mongoose.connect(uri, (err) => {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log('Conectado a ' + uri);
+        }
+    }, {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true
+    });
     
-
-    // console.log(names)
-    // console.log(hrefs)
+    const Schema = mongoose.Schema;
+    const linkSchema = new Schema({
+        season: String,
+        name: String,
+        url: String
+    })
+    const Link = mongoose.model('Link', linkSchema);
+    const newLink = new Link(linksToDB);
+    newLink.save((err) => {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log('Data saved');
+        }
+    });
+    
     await browser.close();
 })();
 
+
+app.listen(port, () => {
+    console.log('App listening on port ' + port);
+});
