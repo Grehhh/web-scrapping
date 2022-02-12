771cbb2671dd4fca6284fb713d877d8ebc626b1c
Graham <grh.mrg@gmail.com>
2022-02-12 20:14:21 +0100

    insert many half-working, credentials by cmd

--- scrapper/index.js
+++ scrapper/index.js
@@ -2,23 +2,34 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 const express = require('express');
 const mongoose = require('mongoose');
+const res = require('express/lib/response');
+const { error } = require('console');
 const app = express();
 const port = 3000;
 
 (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://southparkspainhd.wordpress.com/');
-    const names = await page.$$eval('.menu-item-object-post', name => name.map(n => n.textContent));
-    const hrefs = await page.$$eval('.menu-item-object-post > a', as => as.map(a => a.href));
-    const linksToDB = [];
-    for(let i = 0; i < hrefs.length; i++) {d
-        linksToDB.push({
-            name: names[i],
-            url: hrefs[i]
-        })
-    };
-    const uri = 'mongodb+srv://Graham:lacontrasenya@cluster0.xmw07.mongodb.net/testDB1?retryWrites=true&w=majority'
+
+    const getLinks = async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('https://southparkspainhd.wordpress.com/');
+        const names = await page.$$eval('.menu-item-object-post', name => name.map(n => n.textContent));
+        const hrefs = await page.$$eval('.menu-item-object-post > a', as => as.map(a => a.href));
+        const linksToDB = [];
+        for(let i = 0; i < 3; i++) {
+            linksToDB.push({
+                name: names[i],
+                url: hrefs[i]
+            })
+        };
+        await browser.close();
+        return linksToDB;
+    }
+    
+    const name = process.argv.slice(2,3);
+    const pass = process.argv.slice(3);
+
+    const uri = 'mongodb+srv://' + name + ':' + pass+ '@cluster0.xmw07.mongodb.net/testDB1?retryWrites=true&w=majority'
     mongoose.connect(uri, (err) => {
         if(err) {
             console.log(err);
@@ -32,24 +43,33 @@ const port = 3000;
     
     const Schema = mongoose.Schema;
     const linkSchema = new Schema({
-        season: String,
         name: String,
-        url: String
+        url: String,
+        createdDate: { type: Date, default: Date.now() }
     })
     const Link = mongoose.model('Link', linkSchema);
-    const newLink = new Link(linksToDB);
-    newLink.save((err) => {
-        if(err) {
-            console.log(err);
-        } else {
-            console.log('Data saved');
-        }
-    });
+    const linksToDB = await getLinks();
+    // const linksToDB =  { 
+    //     name: 'name3',
+    //     url: 'name3.url'
+    // }
+    try {
+        const result = await Link.insertMany(linksToDB);
+        console.log('links inserted');
+    } catch(err) {
+        console.error(err);
+    }
+
+    //empty collection
+    // await Link.deleteMany({})        
     
-    await browser.close();
+    app.listen(port, () => {
+        console.log('App listening on port ' + port);
+    });
 })();
 
 
-app.listen(port, () => {
-    console.log('App listening on port ' + port);
-});
+
+
+
+
